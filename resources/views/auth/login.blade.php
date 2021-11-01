d2fd0803a5b46d2028db8b5a1bd2125f89aa1aa2
Anowar Hossain Apurbo <ahosien57@gmail.com>
2021-11-02 00:20:29 +0600

    admin login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,56 +1,81 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+	<title>Log In Page</title>
+	<!--favicon-->
+	<link rel="icon" href="{{ asset('backend/assets/images/favicon-32x32.png')}}" type="image/png" />
+	<!-- loader-->
+	<link href="{{ asset('backend/assets/css/pace.min.css')}}" rel="stylesheet" />
+	<script src="{{ asset('backend/assets/js/pace.min.js')}}"></script>
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="{{ asset('backend/assets/css/bootstrap.min.css')}}" />
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&amp;family=Roboto&amp;display=swap" />
+	<!-- Icons CSS -->
+	<link rel="stylesheet" href="{{ asset('backend/assets/css/icons.css')}}" />
+	<!-- App CSS -->
+	<link rel="stylesheet" href="{{ asset('backend/assets/css/app.css') }}" />
+</head>
 
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
-
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
-
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
-            </div>
-
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+<body class="bg-login">
+	<!-- wrapper -->
+	<div class="wrapper">
+		<div class="section-authentication-login d-flex align-items-center justify-content-center">
+			<div class="row">
+				<div class="col-12 col-lg-10 mx-auto">
+					<div class="card radius-15">
+						<div class="row no-gutters">
+							<div class="col-lg-6">
+								<div class="card-body p-md-5">
+                                    <form action="{{ route('login') }}" method="post">
+                                        @csrf
+									<div class="text-center">
+										<img src="assets/images/logo-icon.png" width="80" alt="">
+										<h3 class="mt-4 font-weight-bold">Welcome Back</h3>
+									</div>
+									<div class="form-group mt-4">
+										<label for="email">Email Address</label>
+										<input type="email" name="email" id="email" class="form-control" placeholder="Enter your email address" />
+									</div>
+									<div class="form-group">
+										<label for="password">Password</label>
+										<input type="password" name="password" id="password" class="form-control" placeholder="Enter your password" />
+									</div>
+									<div class="form-row">
+										<div class="form-group col">
+											<div class="custom-control custom-switch">
+												<input id="remember_me" name="remember" type="checkbox" class="custom-control-input">
+												<label for="remember_me" class="custom-control-label">Remember Me</label>
+											</div>
+										</div>
+										<div class="form-group col text-right"> <a href="authentication-forgot-password.html"><i class='bx bxs-key mr-2'></i>Forget Password?</a>
+										</div>
+									</div>
+									<div class="btn-group mt-3 w-100">
+										<button type="submit" class="btn btn-primary btn-block">Log In</button>
+										<button type="submit" class="btn btn-primary"><i class="lni lni-arrow-right"></i>
+										</button>
+									</div>
+                                </form>
+									<hr>
+									<div class="text-center">
+										<p class="mb-0">Don't have an account? <a href="authentication-register.html">Sign up</a>
+										</p>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-6">
+								<img src="{{ asset('backend/assets/images/login-images/login-frent-img.jpg')}}" class="card-img login-img h-100" alt="...">
+							</div>
+						</div>
+						<!--end row-->
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- end wrapper -->
+</body>
+</html>
