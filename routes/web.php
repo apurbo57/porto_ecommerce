6e5830d80a340e36f05599514bbdab341e4eac3d
Anowar Hossain Apurbo <ahosien57@gmail.com>
2021-11-22 00:05:07 +0600

    test paginate

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\staff\DashboardController;
+use App\Http\Controllers\testController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,7 +22,8 @@ Route::get('/', function () {
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
 // })->middleware(['auth'])->name('dashboard');
-
+Route::get('/test',[testController::class, 'index']);
+Route::post('/test',[testController::class, 'get_data'])->name('get_data');
 
 
 require __DIR__.'/auth.php';
